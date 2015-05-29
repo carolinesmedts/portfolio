958c495f382ae6c8cecb17303b790300c2d7b0a8
carolinesmedts <caroline.smedts@hotmail.com>
2015-05-29 20:30:44 +0000

    Mijn eerste commit

--- index.js
+++ index.js
@@ -1,6 +1,39 @@
 var main = function () {
   "use strict";
-  // hier komt je code
-}
+  $(".link1").on("click", function(){
+  	$(".about").addClass("onzichtbaar");
+  	$(".work").addClass("onzichtbaar");
+  	$(".social").addClass("onzichtbaar");
+  	$(".aboutme").removeClass("onzichtbaar");
+  });
+
+  $(".link2").on("click", function(){
+  	$(".about").addClass("onzichtbaar");
+  	$(".work").addClass("onzichtbaar");
+  	$(".social").addClass("onzichtbaar");
+  	$(".mywork").removeClass("onzichtbaar");
+  }); 
+
+   $(".link3").on("click", function(){
+  	$(".about").addClass("onzichtbaar");
+  	$(".work").addClass("onzichtbaar");
+  	$(".social").addClass("onzichtbaar");
+  	$(".socialmedia").removeClass("onzichtbaar");
+  });
+
+ };
+ 
+ $(".back").on("click", function (){
+ 	$(".aboutme").addClass("onzichtbaar");
+ 	$(".mywork").addClass("onzichtbaar");
+ 	$(".socialmedia").addClass("onzichtbaar");
+ 	$(".about").removeClass("onzichtbaar");
+ 	$(".work").removeClass("onzichtbaar");
+ 	$(".social").removeClass("onzichtbaar");
+ });
+ 
+ 	
+
+
 
 $("document").ready(main);
